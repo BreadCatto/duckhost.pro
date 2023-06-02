99936b7a22ca43276d5496397cc0937b44cf599b
Jay Kumar <90331269+BreadCatto@users.noreply.github.com>
2023-06-02 19:32:54 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+};
+
+console.log("script worki")
